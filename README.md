2f8b21bff0c350683dd55d7aa8b9f2b580f73440
saewoom <swkim@lbstech.net>
2021-10-13 14:35:13 +0900

    commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Image_classification
+
+## Figma에서 Export한 이미지 각 배율에 맞게 자동으로 분류하는 패키지
+
+### Use
+```
+--input, -i <이미지가 위치해 있는 Directory>
+--output, -o <복사한 이미지가 위치할 Directory> Default : input directory
+--separator, -s <배율 앞에 붙는 구분자> Default : @
+```
---
+++ bin/image_classification.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:path/path.dart' as path;
+
+const _kExtensions = ['png', 'jpg', 'svg', 'pdf'];
+
+void main(List<String> arguments) async {
+  var parser = ArgParser();
+  var inputDirectory = Directory.current;
+  var outputDirectory = Directory.current;
+  parser.addOption(
+    'input',
+    abbr: 'i',
+    help: 'The path with the image.',
+  );
+  parser.addOption(
+    'output',
+    abbr: 'o',
+    help: 'The path where the image will be saved.',
+  );
+  parser.addOption('separator',
+      abbr: 's',
+      help: 'Distinguisher for image magnification.',
+      valueHelp: '@',
+      defaultsTo: '@');
+  parser.addFlag(
+    'help',
+    abbr: 'h',
+    help: 'help',
+    negatable: false,
+  );
+
+  final results = parser.parse(arguments);
+
+  if (results.wasParsed('help')) {
+    print(parser.usage);
+    exit(0);
+  }
+  if (results.wasParsed('input')) {
+    inputDirectory = await Directory(results['input']).create();
+  }
+
+  if (!results.options.contains('output')) {
+    outputDirectory = inputDirectory;
+  }
+  if (results.wasParsed('output')) {
+    outputDirectory = await Directory(results['output']).create();
+  }
+
+  var listSync = inputDirectory.listSync();
+
+  var list = listSync
+      .where((element) => _kExtensions.contains(element.path.split('.').last));
+  final ratios = <String, List<FileSystemEntity>>{};
+  final regex = RegExp('(${results['separator']})(.*?)(x)');
+
+  for (final dir in list) {
+    final fileName = dir.path.split('/').last;
+    final split = regex.allMatches(fileName);
+
+    var ratio='';
+
+    for (final element in split) {
+      ratio = '${element.group(2)}${element.group(3)}';
+    }
+    if (fileName.contains('${results['separator']}')) {
+      ratios.update(
+        ratio,
+        (value) => value..add(dir),
+        ifAbsent: () => [dir],
+      );
+    } else {
+      ratios.update(
+        '1x',
+        (value) => value..add(dir),
+        ifAbsent: () => [dir],
+      );
+    }
+  }
+
+  for (final ratio in ratios.entries) {
+    if (ratio.key == '1x') {
+      for (final dir in ratio.value) {
+        final basNameWithExtension = path.basename(dir.path);
+        File(dir.path)
+            .copySync('${outputDirectory.path}/$basNameWithExtension');
+      }
+    } else {
+      var directory =
+          await Directory('${outputDirectory.path}/${ratio.key}').create();
+      for (final dir in ratio.value) {
+        final basNameWithExtension = path.basename(dir.path);
+        var replaceName = basNameWithExtension.replaceAll(regex,'');
+        File(dir.path).copySync('${directory.path}/$replaceName');
+      }
+    }
+  }
+}
